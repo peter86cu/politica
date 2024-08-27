d4ed74bf192b7d04a3d5b3362803229eba1659e0
peter86cu <39494828+peter86cu@users.noreply.github.com>
2024-08-27 00:25:24 -0300

    poiliticas

---
+++ src/main/java/com/online/politica/Mantenimiento.java
@@ -0,0 +1,19 @@
+package com.online.politica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+
+
+
+@SpringBootApplication
+public class Mantenimiento {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Mantenimiento.class, args);
+	}
+
+}
---
+++ src/main/java/com/online/politica/controller/InicioController.java
@@ -0,0 +1,1 @@
+package com.online.politica.controller;import org.springframework.stereotype.Controller;import org.springframework.ui.Model;import org.springframework.web.bind.annotation.GetMapping;@Controllerpublic class InicioController {		@GetMapping({ "/" })	public String showForm(Model modelo)  {				return "index";	}		}
